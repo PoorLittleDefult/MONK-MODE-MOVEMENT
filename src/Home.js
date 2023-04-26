5555cb8834890ba6b016621129e791870c39f399
ForTheWinAlways <82647017+PoorLittleDefult@users.noreply.github.com>
2023-04-26 23:37:04 +1200

    final build p1

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef} from "react";
 import background from "./background.svg"
-import book from "./mock-book.png"
-import handbook from "./handbook.png"
+import handbook from "./course-box.png"
 import TextChanger from './TextChanger';
 import { Link } from "react-router-dom";
 import NavBar from './NavBar';
@@ -38,7 +37,7 @@ function Home(){
                 <div className="h5-container">
                 <h5>Its time for you to take your life into your own hands. No Excuses. You need to become the best version of yourself now.</h5>
                 </div>
-            <a className="button-text-1" target="_blank" href="https://whop.com/"><button className="button-started">CLICK HERE!</button></a>
+            <a className="button-text-1" target="_blank" href=""><button className="button-started">CLICK HERE!</button></a>
         </div>
             {/* <div className="main-mid">
             <h2 className="template-header">Get our FREE Notion template now!</h2>
@@ -54,7 +53,7 @@ function Home(){
                 <h4 className="course-box-h4">Get it Now for $49</h4>
                 <div className="course-buy-box">
                     <div className="container">
-                    <a className="button-text-1" target="_blank" href="https://whop.com/monk-mode-movement  "><button className="button-course">Buy</button></a>
+                    <a className="button-text-1" target="_blank" href=""><button className="button-course">Buy</button></a>
                         <span className="learn-text">Learn More -></span>
                     </div>
                 </div>
@@ -78,14 +77,14 @@ function Home(){
                 <p className="bullet-point">✓ Sleep and rest</p>
             </div>
             <div className="included-wrapper">
-                <h3 className="info-heading">Environment</h3>
-                <p className="bullet-point">✓ Upgrade your environment</p>
-                <p className="bullet-point">✓ Routine Optimization</p>
-                <p className="bullet-point">✓ Sleep and rest</p>
+                <h3 className="info-heading">Mental & Mindset</h3>
+                <p className="bullet-point">✓ Social application</p>
+                <p className="bullet-point">✓ Proven Mental Health fixes</p>
+                <p className="bullet-point">✓ Key Mindset ideas</p>
             </div>
 
-
-
+         </div>
+        <div className="card-container">
         <div class="plan">
 		<div class="inner">
 			<span class="pricing">
@@ -104,6 +103,52 @@ function Home(){
 						</svg>
 					</span>
 					<span>Day to day optimization</span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span>Testosterone <strong>Secrets</strong></span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+                    <span><strong>Boosting </strong>methods</span>
+				</li>
+			</ul>
+			<div class="action">
+			<a class="button" href="#">
+				Get it now!
+			</a>
+			</div>
+		</div>
+        </div>
+
+        <div class="plan">
+		<div class="inner">
+			<span class="pricing">
+				<span>
+					<small>Worth: </small> $99
+				</span>
+			</span>
+			<p class="title">Environment upgrade</p>
+			<p class="info">Ultimate guide day to day on tesostrone optimization</p>
+			<ul class="features">
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span><strong>10</strong> environment hacks</span>
 				</li>
 				<li>
 					<span class="icon">
@@ -132,14 +177,15 @@ function Home(){
 		</div>
         </div>
 
+        
         <div class="plan">
 		<div class="inner">
 			<span class="pricing">
 				<span>
-					<small>Worth: </small> $199
+					<small>Worth: </small> $149
 				</span>
 			</span>
-			<p class="title">Testosterone Lifestyle</p>
+			<p class="title">Health 101</p>
 			<p class="info">Ultimate guide day to day on tesostrone optimization</p>
 			<ul class="features">
 				<li>
@@ -158,7 +204,7 @@ function Home(){
 							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
 						</svg>
 					</span>
-					<span>Testosterone <strong>Secrets</strong></span>
+					<span>Nutrition <strong>Cheat codes</strong></span>
 				</li>
 				<li>
 					<span class="icon">
@@ -167,7 +213,7 @@ function Home(){
 							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
 						</svg>
 					</span>
-					<span>Boosting methods</span>
+					<span><strong>Boosting </strong>methods</span>
 				</li>
 			</ul>
 			<div class="action">
